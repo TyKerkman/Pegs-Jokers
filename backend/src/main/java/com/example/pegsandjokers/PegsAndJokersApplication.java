cc84c3fe9bee944b922731a8f575edadf69e6d2b
TageZ <tagezerby@gmail.com>
2024-03-23 14:13:46 -0500

    Minor changes

--- backend/src/main/java/com/example/pegsandjokers/PegsAndJokersApplication.java
+++ backend/src/main/java/com/example/pegsandjokers/PegsAndJokersApplication.java
@@ -1,13 +1,13 @@
-//package com.example.pegsandjokers;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class PegsAndJokersApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(PegsAndJokersApplication.class, args);
-//	}
-//
-//}
+package com.example.pegsandjokers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegsAndJokersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegsAndJokersApplication.class, args);
+	}
+
+}
