2e550f38e5c209f034ce37fae17e9ba79ceb02a5
TageZ <tagezerby@gmail.com>
2024-03-22 23:06:26 -0500

    Updated Backend

--- backend/src/main/java/com/example/pegsandjokers/Peg.java
+++
@@ -1,5 +0,0 @@
-package com.example.pegsandjokers;
-
-public class Peg {
-
-}
--- backend/src/main/java/com/example/pegsandjokers/Value.java
+++
@@ -1,6 +0,0 @@
-package com.example.pegsandjokers;
-
-public enum Value {
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, JOKER
-
-}
