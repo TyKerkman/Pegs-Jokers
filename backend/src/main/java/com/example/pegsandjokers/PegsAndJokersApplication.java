9de4e50e09b7678dc43210b6abad9bb7db6e7d36
TageZ <tagezerby@gmail.com>
2024-04-09 00:34:38 -0500

    Readded lost springboot application runner.

---
+++ backend/src/main/java/com/example/pegsandjokers/PegsAndJokersApplication.java
@@ -0,0 +1,13 @@
+package com.example.pegsandjokers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegsAndJokersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PegsAndJokersApplication.class, args);
+    }
+
+}
