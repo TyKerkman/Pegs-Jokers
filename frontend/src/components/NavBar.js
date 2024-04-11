40c32747fb2650e6fa399a4655999c5675d2ef00
TageZ <tagezerby@gmail.com>
2024-04-11 16:15:03 -0500

    Updates to navbar.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import "../Styling.css";
+import homeIcon from "../images/home-icon.svg";
 
 const NavBar = ({ title }) => {
     return (
       <nav className="navBar">
         <button className="navButton" onClick={() => window.location.href = '/'}>
-          Home
+            <img src={homeIcon} />
         </button>
         <h1 className="navTitle">{title}</h1>
       </nav>
