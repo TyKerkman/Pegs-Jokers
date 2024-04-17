36cf4246477fc787ae749cab797fa9bc7c528c35
TyKerkman <tkermkan3@huskers.unl.edu>
2024-04-17 13:39:42 -0500

    Changes to hole and board segment

--- frontend/src/components/BoardSegment.js
+++ frontend/src/components/BoardSegment.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Hole from './Hole'; 
 
-const BoardSpot = ({ orientation = "vertical", background_color }) => {
+const BoardSegment = ({background_color, hole }) => {
+    // const segmentStyle = {
+    //     background: background_color,
+    //     display: 'flex',
+    //     flexDirection: orientation === "vertical" ? 'column' : 'row',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     width: orientation === "vertical" ? '40px' : '720px',
+    //     height: orientation === "vertical" ? '720px' : '40px'
+    // };
 
     const holeWrapperStyle = {
+        background: background_color,
         width: '40px',
         height: '40px',
         display: 'flex',
@@ -11,8 +21,11 @@ const BoardSpot = ({ orientation = "vertical", background_color }) => {
         alignItems: 'center'
     };
 
+    const player_colors = ['red', 'fuscia', 'green', 'blue']
+
     return (
         <div style={holeWrapperStyle}>
+            <Hole piece={hole.peg} color={player_colors[hole.numPlayer]}/>
         </div>
     );
 };
