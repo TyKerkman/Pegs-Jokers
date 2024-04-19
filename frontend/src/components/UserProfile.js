0c8665c7e1f20103bb7f892b5d65fa4e43fe5e8e
Banderson03 <112013412+Banderson03@users.noreply.github.com>
2024-04-19 15:29:34 -0500

    Committing Profile Picture Upload and Firebase Storage

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { auth, database } from "../firebase";
+import { auth, database, upload } from "../firebase";
 import { ref, get, child } from "firebase/database";
+
 
 const UserProfile = () => {
   const [userData, setUserData] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [photoURL, setPhotoUrl] = useState("https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg")
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -14,6 +18,9 @@ const UserProfile = () => {
           const snapshot = await get(child(userRef, "/"));
           if (snapshot.exists()) {
             setUserData(snapshot.val());
+            if (user?.photoURL) {
+              setPhotoUrl(user.photoURL)
+            }
           } else {
             console.log("No user data available");
           }
@@ -28,11 +35,29 @@ const UserProfile = () => {
     fetchUserData();
   }, []);
 
+  function handleImage(e) {
+    if(e.target.files[0]) {
+      setPhoto(e.target.files[0])
+      console.log("image");
+    }
+  }
+
+  function handleUpload() {
+    console.log("upload");
+    upload(photo, auth.currentUser, setLoading)
+  }
+
+
   return (
     <div className="user-profile">
       {userData ? (
         <div>
           <h2>User Profile</h2>
+          <div className="image-field">
+            <input type="file" onChange={handleImage} />
+            <button disabled={loading} onClick={handleUpload}>Upload</button>
+            <img src={photoURL} alt="Profile Picture" className="profile-pic" />
+          </div>
           <p>Email: {userData.email}</p>
           <p>Display Name: {userData.name}</p>
           <p>Last Login: {new Date(userData.last_login).toLocaleString()}</p>
