6413bb6888d117c051cef7242627f505fc282162
TyKerkman <112013890+TyKerkman@users.noreply.github.com>
2024-04-21 20:37:06 -0500

    Frontend testing (#41)
    * Removed unused imports
    
    * Started testing suite
    
    * Finished Game testing

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ import homeIcon from "../images/home-icon.svg";
 const NavBar = ({ title }) => {
     return (
       <nav className="navBar">
-        <button className="navButton" onClick={() => window.location.href = '/home'}>
+        <button data-testid="navbar-button" className="navButton" onClick={() => window.location.href = '/home'}>
             <img src={homeIcon} />
         </button>
         <h1 className="navTitle">{title}</h1>
